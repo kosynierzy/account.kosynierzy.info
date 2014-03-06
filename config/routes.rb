76480decb13ff08ac6a839796a2fcaaf27eff622
Jan Dudulski <jan@dudulski.pl>
2014-03-06 21:30:09 +0100

    Remove destroy account action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Kosynierzy::Application.routes.draw do
   mount BeanstalkdView::Server, at: '/beanstalkd'
 
-  devise_for :users
+  devise_for :users, skip: :registrations
 
   devise_scope :user do
+    resource :registration,
+      only: [:new, :create, :edit, :update],
+      path: 'users',
+      path_names: { new: 'sign_up' },
+      controller: 'devise/registrations',
+      as: :user_registration do
+        get :cancel
+      end
+
     authenticated :user do
       root to: 'devise/registrations#edit', as: :authenticated_user
     end
