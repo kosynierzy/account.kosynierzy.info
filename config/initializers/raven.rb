1ca2ba652667f8231bfc9da503cf8cb28ba09758
Jan Dudulski <jan@dudulski.pl>
2015-01-09 12:05:05 +0100

    [feat] Do not monitor development errors

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,4 +2,6 @@ require 'raven'
 
 Raven.configure do |config|
   config.dsn = Figaro.env.raven_url
+  config.environments = [Rails.env] - %w[ development test ]
+  config.tags = { environment: Rails.env }
 end
